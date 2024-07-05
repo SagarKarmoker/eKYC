e569dc27d49bca807243df2662b2554a7bc898fe
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-07-05 22:17:43 +0600

    push to github

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -181,9 +181,9 @@ function SignUp() {
   return (
     <div className="bg-white rounded-lg py-5">
       <div className="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
-        <div class="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
-          <div class="flex items-center justify-center w-full lg:p-12">
-            <div class="flex items-center xl:p-10">
+        <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
+          <div className="flex items-center justify-center w-full lg:p-12">
+            <div className="flex items-center xl:p-10">
               <form
                 className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl"
                 onSubmit={handleSubmit}
