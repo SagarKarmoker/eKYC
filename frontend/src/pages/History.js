0eba7774b771379625f656e9f025d50266f4ad9f
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-07-12 18:15:23 +0600

    tx history design updated

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -1,48 +1,87 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+    Box,
+    Flex,
+    Text,
+    VStack,
+    HStack,
+    Container,
+    Divider,
+    Icon,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, InfoIcon } from "@chakra-ui/icons";
 
 function History() {
-    const [history, setHistory] = useState([])
+    const [history, setHistory] = useState([]);
 
     useEffect(() => {
-        axios.post('http://localhost:3001/getAllTxs', {
-            nid: localStorage.getItem('nidNumber')
-        })
-            .then(res => {
-                setHistory(res.data)
+        axios
+            .post("http://localhost:3001/getAllTxs", {
+                nid: localStorage.getItem("nidNumber"),
             })
-            .catch(err => {
-                console.log(err)
+            .then((res) => {
+                setHistory(res.data);
             })
-    }, [])
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     return (
-        <>
-            <div className="bg-white rounded-lg py-5">
-                <div className="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
-                    <div class="flex-col justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
-                        <div class="flex items-center justify-center w-full lg:p-12">
-                            <h2 class="mb-12 text-4xl font-extrabold text-dark-grey-900">Transaction History</h2>
-                        </div>
-                        <div>
-                            {history.map((tx, index) => (
-                                <div key={index} className="flex items-center justify-between w-full p-5 my-2 bg-gray-100 rounded-lg">
-                                    <p className="text-lg font-semibold text-dark-grey-900 text-center">{tx.reason}</p>
-                                    <div>
-                                        <p className="text-lg font-semibold text-dark-grey-900">Transaction Hash: {tx.hash}</p>
-                                        <p className="text-lg font-semibold text-dark-grey-900">From: {tx.from}</p>
-                                        <p className="text-lg font-semibold text-dark-grey-900">To: {tx.to}</p>
-                                        <p className="text-lg font-semibold text-dark-grey-900">Nonce: {tx.nonce}</p>
-                                        <p className="text-lg font-semibold text-dark-grey-900">Gas Price: {tx.gasPrice}</p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+        <Box className="bg-white rounded-lg py-5 mt-20">
+            <Container className="bg-white rounded-lg p-5" maxW="container.lg">
+                <Flex justify="center" mb={8}>
+                    <Text fontSize="4xl" fontWeight="extrabold" color="gray-900">
+                        Transaction History
+                    </Text>
+                </Flex>
+                <VStack spacing={4} align="stretch">
+                    {history.map((tx, index) => (
+                        <Box
+                            key={index}
+                            className="bg-gray-100 rounded-lg p-5 w-full"
+                            boxShadow="md"
+                        >
+                            <HStack spacing={4} justify="space-between">
+                                <Icon as={CheckCircleIcon} color="green.500" boxSize={8} />
+                                <Text
+                                    fontSize="lg"
+                                    fontWeight="bold"
+                                    className="text-center text-dark-grey-900"
+                                >
+                                    {tx.reason}
+                                </Text>
+                            </HStack>
+                            <Divider my={4} />
+                            <VStack align="start" spacing={2}>
+                                <Text fontSize="md" fontWeight="semibold" color="gray-700">
+                                    <InfoIcon mr={2} />
+                                    Transaction Hash: {tx.hash}
+                                </Text>
+                                <Text fontSize="md" fontWeight="semibold" color="gray-700">
+                                    <InfoIcon mr={2} />
+                                    From: {tx.from}
+                                </Text>
+                                <Text fontSize="md" fontWeight="semibold" color="gray-700">
+                                    <InfoIcon mr={2} />
+                                    To: {tx.to}
+                                </Text>
+                                <Text fontSize="md" fontWeight="semibold" color="gray-700">
+                                    <InfoIcon mr={2} />
+                                    Nonce: {tx.nonce}
+                                </Text>
+                                <Text fontSize="md" fontWeight="semibold" color="gray-700">
+                                    <InfoIcon mr={2} />
+                                    Gas Price: {tx.gasPrice}
+                                </Text>
+                            </VStack>
+                        </Box>
+                    ))}
+                </VStack>
+            </Container>
+        </Box>
+    );
 }
 
-export default History
+export default History;
